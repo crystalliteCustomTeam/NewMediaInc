b652bcf7d0a29f02d6919440655db2064f713d5c
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2025-07-04 22:58:50 +0500

    new changes by chachu

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import partners from "media/stripe_logo.png";
+import partners from "media/payment-partners.png";
 import methods from "media/payment-methods.png";
 
 const Footer = () => {
@@ -84,12 +84,40 @@ const Footer = () => {
             </div>
             <div className="xl:col-span-3 lg:col-span-4 sm:col-span-6 col-span-12">
               <h4 className="md:text-[30px] text-xl leading-normal font-semibold mb-3 text-[#dee56f]">
-                Office Timings
+                Locations
               </h4>
-              <ul className="text-[#8F8F8F] text-sm">
-                <li>Office Hours</li>
-                <li>Mon-Fri</li>
-                <li>8:00AM – 6:00PM</li>
+              <ul className="text-[#8F8F8F] text-sm flex flex-col gap-y-2">
+                <li className="flex items-center gap-x-2">
+                  <span className="block">
+                    <svg
+                      fill="#dee56f"
+                      width={10}
+                      height={10}
+                      aria-hidden="true"
+                      className="e-font-icon-svg e-fas-map-marker-alt"
+                      viewBox="0 0 384 512"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path>
+                    </svg>
+                  </span>
+                  9518 Mykawa Rd Houston, TX 77048-1313 United States</li>
+                <li className="flex items-center gap-x-2">
+                  <span className="block">
+                    <svg
+                      fill="#dee56f"
+                      width={10}
+                      height={10}
+                      aria-hidden="true"
+                      className="e-font-icon-svg e-fas-map-marker-alt"
+                      viewBox="0 0 384 512"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path>
+                    </svg>
+                  </span>
+                  5900 Balcones Dr STE 100 Austin, TX. 78731</li>
+
               </ul>
             </div>
             <div className="lg:col-span-4 sm:col-span-6 col-span-12">
@@ -188,7 +216,7 @@ const Footer = () => {
                 </span>
                 <span className="block">info@Infinitimediainc.com</span>
               </a>
-              <a
+              {/* <a
                 href="https://g.co/kgs/AWxFQ7f"
                 target="_blank"
                 className="flex items-center gap-x-2 text-[13px] leading-tight font-light"
@@ -207,7 +235,7 @@ const Footer = () => {
                   </svg>
                 </span>
                 <span className="block">5900 Balcones Dr STE 100 Austin, TX. 78731</span>
-              </a>
+              </a> */}
             </div>
             <div>
               <p className="text-[13px] leading-tight text-center font-light">
